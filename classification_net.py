fe130800f24ba9fe8b492384a21e1834162ae1d0
Bijan Nicholas Massoumi <bnmassou@pc21.cs.ucdavis.edu>
2017-06-04 03:20:20 -0700

    added forward propagation

--- classification_net.py
+++ classification_net.py
@@ -1,11 +1,24 @@
-from layer import *
+import numpy as np
+import preprocess
 
 class ClassificationNetwork:
     def __init__(self,num_inputs,num_classes,num_hidden_layers,nodes_per_layer):
+        self.num_layers = num_hidden_layers
         self.weights = allocate_inital_weights(num_hidden_layers,\
                                             num_inputs,num_classes)
 
+    def forward_propagation(self,image_data):
+        activations = []
+        activations.append(np.concatenate((np.array([1]),image_data)))
+        for i in range(0,self.num_layers+1):
+            curr_layer = activations[-1].transpose()
+            z = self.weights[i].dot(curr_layer)
+            activations.append(sigmoid_function(z))
+        return activations
 
+
+    def sigmoid_function(z):
+        return 1. / (1. + np.exp(-z))
 
     def allocate_inital_weights(num_hidden_layers,num_inputs,num_classes):
         weights = []
--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,18 @@
- import numpy as np
- import csv
- 
- def load_images(filename):
+import numpy as np
+import csv
+
+def load_images(filename):
     with open(filename) as f:
-        f = list(csv.reader(f)[1:]
+        f = list(csv.reader(f))[1:]
         y = [None]*len(f)
         images = [None]*len(f)
         for i,row in enumerate(f):
             images[i] = np.array(f[i][1:],dtype = 'float64')
             y[i] = np.array(get_correct_target(int(f[i][0])),dtype = 'float64')
-            
-            
-            
+        return np.array(images), np.array(y)
+
+
+
 def get_correct_target(num):
     return {
         0:[1,0,0,0,0,0,0,0,0,0],
@@ -25,5 +26,3 @@ def get_correct_target(num):
         8:[0,0,0,0,0,0,0,0,1,0],
         9:[0,0,0,0,0,0,0,0,0,1]
     }[num]
-    
- 
