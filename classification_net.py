a487bca5a363960d6cdb78414c5303cc38ae92b0
Bijan Nicholas Massoumi <bnmassou@pc21.cs.ucdavis.edu>
2017-06-03 21:07:03 -0700

    added some stuff

--- classification_net.py
+++ classification_net.py
@@ -1,2 +1,21 @@
 from layer import *
 
+class ClassificationNetwork:
+    def __init__(self,num_inputs,num_classes,num_hidden_layers,nodes_per_layer):
+        self.weights = allocate_inital_weights(num_hidden_layers,\
+                                            num_inputs,num_classes)
+
+
+
+    def allocate_inital_weights(num_hidden_layers,num_inputs,num_classes):
+        weights = []
+        for i in range(0,num_layers):
+            if i == 0:
+                weights.append(np.random.randn(nodes_per_layer,\
+                                                    num_inputs + 1))
+            else:
+                weights.append(np.random.randn(nodes_per_layer,\
+                                                nodes_per_layer + 1))
+        self.weights.append(np.random.randn(num_classes,nodes_per_layer + 1))
+
+        return weights
---
+++ preprocess.py
@@ -0,0 +1,29 @@
+ import numpy as np
+ import csv
+ 
+ def load_images(filename):
+    with open(filename) as f:
+        f = list(csv.reader(f)[1:]
+        y = [None]*len(f)
+        images = [None]*len(f)
+        for i,row in enumerate(f):
+            images[i] = np.array(f[i][1:],dtype = 'float64')
+            y[i] = np.array(get_correct_target(int(f[i][0])),dtype = 'float64')
+            
+            
+            
+def get_correct_target(num):
+    return {
+        0:[1,0,0,0,0,0,0,0,0,0],
+        1:[0,1,0,0,0,0,0,0,0,0],
+        2:[0,0,1,0,0,0,0,0,0,0],
+        3:[0,0,0,1,0,0,0,0,0,0],
+        4:[0,0,0,0,1,0,0,0,0,0],
+        5:[0,0,0,0,0,1,0,0,0,0],
+        6:[0,0,0,0,0,0,1,0,0,0],
+        7:[0,0,0,0,0,0,0,1,0,0],
+        8:[0,0,0,0,0,0,0,0,1,0],
+        9:[0,0,0,0,0,0,0,0,0,1]
+    }[num]
+    
+ 
