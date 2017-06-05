25bd6b8d1885ab511711a19e7f4c926f67db232a
Dan <dvela28@gmail.com>
2017-06-04 18:15:10 -0700

    Fixed condition

--- weight_layer.py
+++ weight_layer.py
@@ -4,7 +4,7 @@ import activation_methods as am
 class WeightLayer:
     """Class to store weights from layers i to j, and biases in layer j."""
     def __init__(self, num_nodes, num_prev_nodes):
-        if next_layer == 0:
+        if num_prev_nodes == 0:
             raise ValueError('Invalid number of nodes in previous layer')
         self.num_nodes = num_nodes
         self.weights = np.random.randn(num_prev_nodes, num_nodes) / np.sqrt(num_nodes)
