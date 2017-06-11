3ea042beec414ef2191e933763f0611289c3acb8
Dan <dvela28@gmail.com>
2017-06-11 02:35:30 -0700

    Added momentum method

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     train,target = p.load_images('train.csv')
     test,target_test = p.load_images('test.csv')
     net = cn.ClassificationNetwork([train.shape[1],25,10],Activations.sigmoid)
-    net.train_network(train,target, debug = True)
+    net.train_network(train,target,epochs=10,reg_strength=10,learning_rate=10,batch_size=1000,momentum=0.95,debug = False)
     print(net.compute_accuracy(test,target_test))
--- weight_layer.py
+++ weight_layer.py
@@ -4,10 +4,15 @@ import activation_methods as am
 class WeightLayer:
     """Class to store weights from layers i to j, and biases in layer j."""
     def __init__(self, num_nodes, num_prev_nodes):
-        #adding an extra column to weights because im combining the biases into the weight matrix
         if num_prev_nodes == 0:
             raise ValueError('Invalid number of nodes in previous layer')
+        self.velocity = np.zeros((num_prev_nodes, num_nodes))
         self.biases = np.zeros((1, num_nodes))
         self.weights = np.random.randn(num_prev_nodes, num_nodes) / np.sqrt(num_prev_nodes)
+
+    def nesterov_momentum(self, momentum, learning_rate, gradient):
+        velocity_prev = self.velocity
+        self.velocity = momentum * self.velocity - learning_rate * gradient
+        return (-momentum * velocity_prev + (1 + momentum) * self.velocity)
         
         
