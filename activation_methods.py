Fixed bug in derivative functions

activation_methods.py
@@ -2,17 +2,17 @@ import numpy as np
 from enum import Enum
 
 class Activations(Enum):
-    def sigmoid(self, z, derivate = False):
+    def sigmoid(z, derivate = False):
         if derivate:
-            return self.sigmoid(z) * (1 - self.sigmoid(z))
+            return z * (1. - z)
         else:
             return 1. / (1. + np.exp(-z))
-    def tanh(self, z, derivate = False):
+    def tanh(z, derivate = False):
         if derivate:
-            return (1. - np.power(self.tanh(z), 2))
+            return (1. - np.power(z, 2))
         else:
             return np.tanh(z)
-    def relu(self, z, derivate = False):
+    def relu(z, derivate = False):
         if derivate:
             if z < 0:
                 return 0.

classification_net.py
@@ -71,8 +71,8 @@ class ClassificationNetwork:
         """ Runs network forward and returns activations from layers """
         activation = [image_batch]
         for layer in self.layers[:-1]:
-            activation.append(self.__activation_function(self, np.dot(activation[-1] ,\
-                                                layer.weights ) + layer.biases))
+            activation.append(self.__activation_function(np.dot(activation[-1] ,\
+                                                layer.weights) + layer.biases))
         #Softmax activation in last layer for best results
         activation.append(self.softmax(np.dot(activation[-1], self.layers[-1].weights) + \
                                                 self.layers[-1].biases))